4a97cc70cbff1654b4ce4923d85a6c16638f012e
Thibault Polge <thibault@thb.lt>
2022-12-11 15:24:24 +0100

    Don't check child frame names for uniqueness

--- sway.el
+++ sway.el
@@ -7,7 +7,7 @@
 ;;
 ;; Keywords: frames
 ;; Homepage: https://github.com/thblt/sway.el
-;; Version: 0.5
+;; Version: 0.6
 ;; Package-Requires: ((emacs "27.1") (dash "2.18.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -245,7 +245,10 @@ You probably should use `sway-find-window-frame' instead.
 WINDOW is a hash table, typically one of the members of
 `sway-list-windows'."
   ;; A quick sanity check.
-  (let ((names (seq-map (lambda (frame) (frame-parameter frame 'name)) (frame-list))))
+  (let ((names (seq-map (lambda (frame) (frame-parameter frame 'name))
+                        (seq-filter (lambda (f)
+                                      (null (frame-parameter f 'parent-frame)))
+                                    (frame-list))))
     (unless (eq (length names) (length (seq-uniq names)))
       (error "sway.el under pgtk needs frame name to be unique.  Please see README.org.")))
   ;; Then
